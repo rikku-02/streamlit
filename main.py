bf12d0aa43d09cb9d33b473deffb98f50f99c700
Eric Germino <65765604+rikku-02@users.noreply.github.com>
2022-05-13 23:30:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,19 @@ st.title('Rikku.URL Shortener')
 key = st.secrets['API_KEY']
 
 uploaded_files = st.file_uploader("Choose a file", accept_multiple_files=True)
-btnUp = st.button('Upload')
-
 
 for uploaded_file in uploaded_files:
     
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
-    st.write(pathlib.Path(uploaded_file.name).suffix)
+    
+
 
     if btnUp:
-        st.write(owo.upload_files(key, bytes_data))
+        ext = pathlib.Path(uploaded_file.name).suffix
+        st.write(owo.upload_files(key, bytes_data + ext))
+        
+btnUp = st.button('Upload')
     
 
     

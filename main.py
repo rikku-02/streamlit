ca68e2d812619b9fff4987ff1b61412d11857375
Eric Germino <65765604+rikku-02@users.noreply.github.com>
2022-05-13 23:11:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import owo
 st.title('Rikku.URL Shortener')
 key = st.secrets['API_KEY']
 
+uploaded_files = st.file_uploader("Choose a file", accept_multiple_files=True)
 
+for uploaded_file in uploaded_files:
+    st.write("filename:", uploaded_file.name)
+    
 
 url = st.text_input('')
 btn = st.button('Shorten')

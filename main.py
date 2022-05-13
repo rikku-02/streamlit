0cf673041d5d077853ecda40d093b180cb285f59
Eric Germino <65765604+rikku-02@users.noreply.github.com>
2022-05-14 05:34:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,26 +18,32 @@ def auth():
     
 
 def main():
-    IMAGE_URL = "https://ahegao.b-cdn.net/wp-content/uploads/2021/04/Ijiranaide-Nagatoro-san-Episode-1-Nagatoro-Wipes-More-Senpai-Tears.jpg"
-    key = st.secrets['API_KEY']
+    try:
+        IMAGE_URL = "https://ahegao.b-cdn.net/wp-content/uploads/2021/04/Ijiranaide-Nagatoro-san-Episode-1-Nagatoro-Wipes-More-Senpai-Tears.jpg"
+        key = st.secrets['API_KEY']
 
-    st.header('Rikku.File Upload and Url Shortener')
-    st.image(IMAGE_URL)
+        st.header('Rikku.File Upload and Url Shortener')
+        st.image(IMAGE_URL)
 
-    st.subheader('Rikku.File Upload')
-    uploaded_files = st.file_uploader("Upload a File", accept_multiple_files=True)
+        st.subheader('Rikku.File Upload')
+        uploaded_files = st.file_uploader("Upload a File", accept_multiple_files=True)
 
-    btnUp = st.button('Upload')
+        btnUp = st.button('Upload')
 
-    for uploaded_file in uploaded_files:
-        bytes_data = uploaded_file.read()
-        with open(f'{uploaded_file.name}', 'wb') as f: 
-            f.write(bytes_data)
+        for uploaded_file in uploaded_files:
+            bytes_data = uploaded_file.read()
+            with open(f'{uploaded_file.name}', 'wb') as f: 
+                f.write(bytes_data)
     
-    if btnUp:           
-        with st.spinner('Uploading...'):
-            st.write(owo.upload_files(key, uploaded_file.name))
-            st.success('File Uploaded.')
+        if btnUp:           
+            with st.spinner('Uploading...'):
+                st.write(owo.upload_files(key, uploaded_file.name))
+                st.success('File Uploaded.')
+   
+    except UnboundLocalError:
+        st.write('Upload a File')
+            
+     
                
 
  

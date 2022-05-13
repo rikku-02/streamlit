8809f3089aacf8d3e6780156eb37f5e9687c7d8b
Eric Germino <65765604+rikku-02@users.noreply.github.com>
2022-05-14 00:12:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,6 @@ btnUp = st.button('Upload')
 
 
 if uploaded_file is not None:
-     # To read file as bytes:
-     bytes_data = uploaded_file.getvalue()
-     st.write(bytes_data)
-
      # To convert to a string based IO:
      stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
      st.write(stringio)

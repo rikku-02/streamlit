3377be329e7337b0438effb8d84ea270a20f8352
Eric Germino <65765604+rikku-02@users.noreply.github.com>
2022-05-13 22:52:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,21 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+st.title('Rikku. URL Shortener')
+
+key = st.secrets['API_KEY']
+
+
+
+url = st.text_input('')
+btn = st.button('Shorten')
+
+IMAGE_URL = "https://i.pinimg.com/originals/0e/26/1b/0e261bfd97562baad77e39a7c3dad32d.png"
+st.image(IMAGE_URL)
+
+try:
+    if btn:
+        st.write(owo.shorten_urls(key, url))
+
+except ValueError:
+    st.write('Invalid URL, Please input "https://..."')

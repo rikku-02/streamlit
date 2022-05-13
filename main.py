bd9420e4a192a0d85111bbad91b2982e8f95eb68
Eric Germino <65765604+rikku-02@users.noreply.github.com>
2022-05-13 23:00:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ key = st.secrets['API_KEY']
 url = st.text_input('')
 btn = st.button('Shorten')
 
-IMAGE_URL = "https://i.pinimg.com/564x/91/81/c4/9181c4016c719f4e9daf194db019cf3c.jpg"
+IMAGE_URL = "https://ahegao.b-cdn.net/wp-content/uploads/2021/04/Ijiranaide-Nagatoro-san-Episode-1-Nagatoro-Wipes-More-Senpai-Tears.jpg"
 
 
 try:

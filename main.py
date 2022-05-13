e883be141765668ccdacb7a88411d47ef435731d
Eric Germino <65765604+rikku-02@users.noreply.github.com>
2022-05-13 23:22:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,9 @@ btnUp = st.button('Upload')
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
 
     if btnUp:
-        st.write(owo.upload_files(key, uploaded_file))
+        st.write(owo.upload_files(key, bytes_data))
     
 
     

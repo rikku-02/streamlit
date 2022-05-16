c294783424bb04f36d40bdd878481cefb37d27bd
Eric Germino <65765604+rikku-02@users.noreply.github.com>
2022-05-16 09:12:07 +0000

    hehe

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import streamlit as st
 
-st.video('https://gogoanime.co.in/videos/kawaii-dake-ja-nai-shikimori-san-episode-1')
+st.video('https://cdn36.anicache.net/user1342/e838f66fbfe4304eb4c04f499a52bed6/EP.1.v2.1649534944.720p.mp4?token=DI_-qilz7PfXATIHAfDCKA&expires=1652706554&id=184144')

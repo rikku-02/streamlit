f3f4afa7178e0add22f308afa79b190b86f382ad
Eric Germino <65765604+rikku-02@users.noreply.github.com>
2022-05-17 14:03:41 +0000

    test

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
 import streamlit as st
+import aspose.words as aw
+
+
+
+uploaded_files = st.file_uploader("Upload File:", accept_multiple_files=True)
+
+btnUp = st.button('Upload')
+
+for uploaded_file in uploaded_files:
+  bytes_data = uploaded_file.read()
+  with open(f'{uploaded_file.name}', 'wb') as f:
+                f.write(bytes_data)
+
+
+if btnUp:
+  with st.spinner('Uploading...'):
+    fileNames = [uploaded_file.name]
+
+    doc = aw.Document()
+    builder = aw.DocumentBuilder(doc)
+
+    for fileName in fileNames:
+      builder.insert_image(fileName)
+    # Insert a paragraph break to avoid overlapping images.
+      builder.writeln()
+
+      pdf = doc.save("Output.pdf");
+
+      st.download_button(
+       label="Download data as CSV",
+       data=pdf,
+       file_name='output.pdf',
+       mime='text/pdf',
+   )

d12c5b2e1b1f429444abb87a8c99ef93d38f9f40
Eric Germino <65765604+rikku-02@users.noreply.github.com>
2022-05-17 18:03:55 +0000

    add: Image Concatination

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+import img2pdf
 import streamlit as st
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 
-st.image('https://owo.whats-th.is/ufDkZS8.gif')
+st.image('https://owo.whats-th.is/3pBJaga.png')
 st.title('Text Logo Maker by Rikkuチャン')
 
-
-font_style = st.selectbox('Font Style フォント・スタイル :', ('Dream_Catcher', 'Nyctographic', 'Thunderblack', 'Perpetrator_Italic', 'Perpetrator_Regular', 'Sparkles', 'Translator'))
-font_Size = st.number_input('Font Size フォント・サイズ : ', min_value=None, max_value=None, value=20)
+font_style = st.selectbox('Font Style フォント・スタイル :', (
+    'Dream_Catcher', 'Nyctographic', 'Thunderblack', 'Perpetrator_Italic', 'Perpetrator_Regular', 'Sparkles',
+    'Translator'))
+font_Size = st.number_input('Font Size フォント・サイズ : ', min_value=None, max_value=None, value=50)
 width = st.number_input('Canvas Width キャンバス・ウィツ [ CM ]: ', min_value=None, max_value=None, value=10)
 height = st.number_input('Canvas Height キャンバス・ハイト [ CM ]: ', min_value=None, max_value=None, value=5)
-DPI = st.slider('DPI ディー・ピー・アイ [ Default: 72 DPI ]:', 72, 600, 72)
+DPI = st.slider('DPI ディー・ピー・アイ [ Default: 300 DPI ]:', 72, 600, 300)
 bg_color = st.color_picker('Background Color バクグラウンド・カラー :', '#fff')
 font_color = st.color_picker('Font Color フォント・カラー :', '#000')
 border_color = st.color_picker('Border Color ボーダー・カラー :', '#000')
@@ -36,11 +38,31 @@ img_with_border = ImageOps.expand(img, border=border_weight, fill=border_color)
 # Draw elements
 draw = ImageDraw.Draw(img_with_border)
 
+
 def draw_text(font_size):
     pick_font = f'Fonts/{font_style + ".ttf"}'
     font = ImageFont.truetype(pick_font, int(font_size / (res_y_old / res_y)))
     # x, y = (int(x_cm / f * res_x), int(y_cm / f * res_y))
     draw.text(xy=(img.size[0] / 2, img.size[1] / 2), text=txt, font=font, fill=font_color, anchor='mm')
+
+
+def get_concat_h_repeat(im, column):
+    dst = Image.new('RGB', (im.width * column, im.height))
+    for x in range(column):
+        dst.paste(im, (x * im.width, 0))
+    return dst
+
+
+def get_concat_v_repeat(im, row):
+    dst = Image.new('RGB', (im.width, im.height * row))
+    for y in range(row):
+        dst.paste(im, (0, y * im.height))
+    return dst
+
+
+def get_concat_tile_repeat(im, row, column):
+    dst_h = get_concat_h_repeat(im, column)
+    return get_concat_v_repeat(dst_h, row)
 
 
 if build:
@@ -58,37 +80,37 @@ if build:
                     file_name=txt + '.png',
                     mime="image/png"
                 )
-                btn = st.button('Concatenate Image')
-                if btn:
-                  with open(txt + '.png', "rb") as img:
-                    im_s = img.resize((img.width // 2, img.height // 2))
-                    get_concat_tile_repeat(im_s, 10, 5).save('concat.jpg')
-                    st.image('concat.jpg')
-                  
-                
-
     except (ZeroDivisionError, ValueError, SystemError):
         if txt == '':
             st.warning('Input Text Field')
         else:
             st.warning('Input Valid Parameters')
 
+st.title('For Image Concatenation')
+vertical_row = st.number_input('Number of rows in Vertical: ', min_value=None, max_value=None, value=1)
+horizontal_row = st.number_input('Number of rows in Horizontal: ', min_value=None, max_value=None, value=1)
+btn1 = st.button('Concatenate Image')
+if btn1:
+    try:
+        with st.spinner('Concatenating...'):
+            img_c = Image.open(txt + '.png')
+            im_s = img_c.resize((img_c.width // 2, img_c.height // 2))
+            get_concat_tile_repeat(im_s, vertical_row, horizontal_row).save('concat.jpg')
+            st.image('concat.jpg')
 
+            with open(txt + '.pdf', "wb") as f:
+                f.write(img2pdf.convert('concat.jpg'))
 
-def get_concat_h_repeat(im, column):
-    dst = Image.new('RGB', (im.width * column, im.height))
-    for x in range(column):
-        dst.paste(im, (x * im.width, 0))
-    return dst
+            with open(txt + '.pdf', "rb") as pdf_file:
+                PDF = pdf_file.read()
 
-def get_concat_v_repeat(im, row):
-    dst = Image.new('RGB', (im.width, im.height * row))
-    for y in range(row):
-        dst.paste(im, (0, y * im.height))
-    return dst
+                st.download_button(label="Download PDF",
+                                   data=PDF,
+                                   file_name=txt + '.pdf',
+                                   mime='application/octet-stream')
 
-
-def get_concat_tile_repeat(im, row, column):
-    dst_h = get_concat_h_repeat(im, column)
-    return get_concat_v_repeat(dst_h, row)
+    except ValueError:
+        st.warning('0 Value is not allowed to concatinate the image.')
+    except FileNotFoundError:
+        st.warning('Please Build your image first!')
 

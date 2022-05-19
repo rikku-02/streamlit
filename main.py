50213e0ecaaa9e6338df2df84ce267d78b50222f
Eric Germino <65765604+rikku-02@users.noreply.github.com>
2022-05-19 20:03:11 +0000

    test

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ app = MultiApp()
 st.image('https://owo.whats-th.is/3pBJaga.png')
 st.title('Text Logo Maker by Rikkuチャン')
 
-app.add_app("Home", home.app)
-app.add_app("Create", create.app)
-app.add_app("Upload your Custom Design", custom.app)
+with st.sidebar:
+  app.add_app("Home", home.app)
+  app.add_app("Create", create.app)
+  app.add_app("Upload your Custom Design", custom.app)
 
 
 # The main app

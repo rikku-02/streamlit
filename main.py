526651451cde4bac5b157e1cf6cab0c688dcadec
Eric Germino <65765604+rikku-02@users.noreply.github.com>
2022-05-19 16:43:47 +0000

    Error Handling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from pytube import YouTube
+from pytube.exceptions import RegexMatchError
 import datetime
 from moviepy.editor import *
 import os
@@ -15,26 +16,30 @@ url = st.text_input(label='URL: ')
 convert = st.button('Convert')
 
 if convert:
-    yti = YouTube(url)
-    st.subheader(yti.title)
-    st.image(yti.thumbnail_url, width=200)
-    sec = yti.length
-    res = datetime.timedelta(seconds=sec)
-    st.text(f'Duration: {res} ')
+    try:
+        yti = YouTube(url)
+        st.subheader(yti.title)
+        st.image(yti.thumbnail_url, width=200)
+        sec = yti.length
+        res = datetime.timedelta(seconds=sec)
+        st.text(f'Duration: {res} ')
 
-    with st.spinner('Converting...'):
-        yt = YouTube(url)
+        with st.spinner('Converting...'):
+            yt = YouTube(url)
 
-        yt.streams.filter(only_audio=True)
-        stream = yt.streams.get_by_itag(140)
+            yt.streams.filter(only_audio=True)
+            stream = yt.streams.get_by_itag(140)
 
-        freshDownload = stream.download(filePath)
+            freshDownload = stream.download(filePath)
 
-        basePath, extension = os.path.splitext(freshDownload)
+            basePath, extension = os.path.splitext(freshDownload)
 
-        video = AudioFileClip(os.path.join(basePath + ".mp4"))
-        video.write_audiofile(os.path.join(basePath + ".mp3"))
+            video = AudioFileClip(os.path.join(basePath + ".mp4"))
+            video.write_audiofile(os.path.join(basePath + ".mp3"))
 
-        with open(os.path.join(basePath + ".mp3"), 'rb') as f:
-            st.success('Success!')
-            st.download_button('Download Mp3', f, file_name=yt.title + '.mp3')
+            with open(os.path.join(basePath + ".mp3"), 'rb') as f:
+                st.success('Success!')
+                st.download_button('Download', f, file_name=yt.title + '.mp3')
+
+    except RegexMatchError:
+        st.warning('Please input a valid URL!')

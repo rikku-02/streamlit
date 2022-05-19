e60e010d427eaef6d6ccb44306012b058209fba0
Eric Germino <65765604+rikku-02@users.noreply.github.com>
2022-05-19 20:07:54 +0000

    Side Bar Implementation

--- app/home.py
+++ app/home.py
@@ -3,5 +3,5 @@ import streamlit as st
 def app():
     code = '''
     def Hello():
-        return "Go to Navigation to continue"'''
+        return "Go to Side Bar Navigation to continue"'''
     st.code(code, language='python')

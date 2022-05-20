c2f442bc46b13cb77c5d4495a4a72f96ec242160
Eric Germino <65765604+rikku-02@users.noreply.github.com>
2022-05-20 01:42:49 +0000

    new side bar

---
+++ apps/ytmp3.py
@@ -0,0 +1,52 @@
+import streamlit as st
+from pytube import YouTube
+from pytube.exceptions import RegexMatchError
+import datetime
+from moviepy.editor import *
+import os
+
+# Dev-R
+
+def app():
+  filePath = ''
+
+url = st.text_input(label='URL: ')
+convert = st.button('Convert')
+code = """
+def Release_Notes():
+    YouTube_to_MP4 = 'Hmm..'"""
+
+st.code(code, language='python')
+if url == '':
+    pass
+
+else:
+    try:
+        yti = YouTube(url)
+        st.subheader(yti.title)
+        st.image(yti.thumbnail_url, width=200)
+        sec = yti.length
+        res = datetime.timedelta(seconds=sec)
+        st.text(f'Duration: {res} ')
+        # convert = st.button('Convert')
+
+        if convert:
+            with st.spinner('Converting...'):
+                yt = YouTube(url)
+
+                yt.streams.filter(only_audio=True)
+                stream = yt.streams.get_by_itag(140)
+
+                freshDownload = stream.download(filePath)
+
+                basePath, extension = os.path.splitext(freshDownload)
+
+                video = AudioFileClip(os.path.join(basePath + ".mp4"))
+                video.write_audiofile(os.path.join(basePath + ".mp3"))
+
+                with open(os.path.join(basePath + ".mp3"), 'rb') as f:
+                    st.success('Successful! Download Now ⬇')
+                    st.download_button('Download Mp3', f, file_name=yt.title + '.mp3')
+
+    except RegexMatchError:
+        st.warning('Please input a valid URL!')
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import streamlit as st
-from pytube import YouTube
-from pytube.exceptions import RegexMatchError
-import datetime
-from moviepy.editor import *
-import os
+from multiapp import MultiApp
+from apps import ytmp3
 
 # Dev-R
 st.set_page_config(
@@ -19,47 +16,7 @@ st.set_page_config(
 )
 
 st.image('https://owo.whats-th.is/8G7k3AX.png')
-st.header('YouTube to Mp3 Converter')
+st.header('YouTube to Mp3/Mp4 Converter')
 
-filePath = ''
+app.add_app("YT_Convert to Mp3", ytmp3.app)
 
-url = st.text_input(label='URL: ')
-convert = st.button('Convert')
-code = """
-def Release_Notes():
-    YouTube_to_MP4 = 'Hmm..'"""
-
-st.code(code, language='python')
-if url == '':
-    pass
-
-else:
-    try:
-        yti = YouTube(url)
-        st.subheader(yti.title)
-        st.image(yti.thumbnail_url, width=200)
-        sec = yti.length
-        res = datetime.timedelta(seconds=sec)
-        st.text(f'Duration: {res} ')
-        # convert = st.button('Convert')
-
-        if convert:
-            with st.spinner('Converting...'):
-                yt = YouTube(url)
-
-                yt.streams.filter(only_audio=True)
-                stream = yt.streams.get_by_itag(140)
-
-                freshDownload = stream.download(filePath)
-
-                basePath, extension = os.path.splitext(freshDownload)
-
-                video = AudioFileClip(os.path.join(basePath + ".mp4"))
-                video.write_audiofile(os.path.join(basePath + ".mp3"))
-
-                with open(os.path.join(basePath + ".mp3"), 'rb') as f:
-                    st.success('Successful! Download Now ⬇')
-                    st.download_button('Download Mp3', f, file_name=yt.title + '.mp3')
-
-    except RegexMatchError:
-        st.warning('Please input a valid URL!')

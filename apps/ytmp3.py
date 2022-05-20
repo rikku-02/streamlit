e4c365711bd1e9e1c477eb5d36f0b45da0b31c02
Eric Germino <65765604+rikku-02@users.noreply.github.com>
2022-05-20 01:48:46 +0000

    Code Changes

--- apps/ytmp3.py
+++ apps/ytmp3.py
@@ -8,45 +8,46 @@ import os
 # Dev-R
 
 def app():
-  filePath = ''
+    filePath = ''
 
-url = st.text_input(label='URL: ')
-convert = st.button('Convert')
-code = """
-def Release_Notes():
-    YouTube_to_MP4 = 'Hmm..'"""
+    url = st.text_input(label='URL: ')
+    convert = st.button('Convert')
+    code = """
+    def Release_Notes():
+        YouTube_to_MP4 = 'Hmm..'"""
 
-st.code(code, language='python')
-if url == '':
-    pass
+    st.code(code, language='python')
+    if url == '':
+        pass
 
-else:
-    try:
-        yti = YouTube(url)
-        st.subheader(yti.title)
-        st.image(yti.thumbnail_url, width=200)
-        sec = yti.length
-        res = datetime.timedelta(seconds=sec)
-        st.text(f'Duration: {res} ')
-        # convert = st.button('Convert')
+    else:
+        try:
+            yti = YouTube(url)
+            st.subheader(yti.title)
+            st.image(yti.thumbnail_url, width=200)
+            sec = yti.length
+            res = datetime.timedelta(seconds=sec)
+            st.text(f'Duration: {res} ')
+            # convert = st.button('Convert')
 
-        if convert:
-            with st.spinner('Converting...'):
-                yt = YouTube(url)
+            if convert:
+                with st.spinner('Converting...'):
+                    yt = YouTube(url)
 
-                yt.streams.filter(only_audio=True)
-                stream = yt.streams.get_by_itag(140)
+                    yt.streams.filter(only_audio=True)
+                    stream = yt.streams.get_by_itag(140)
 
-                freshDownload = stream.download(filePath)
+                    freshDownload = stream.download(filePath)
 
-                basePath, extension = os.path.splitext(freshDownload)
+                    basePath, extension = os.path.splitext(freshDownload)
 
-                video = AudioFileClip(os.path.join(basePath + ".mp4"))
-                video.write_audiofile(os.path.join(basePath + ".mp3"))
+                    video = AudioFileClip(os.path.join(basePath + ".mp4"))
+                    video.write_audiofile(os.path.join(basePath + ".mp3"))
 
-                with open(os.path.join(basePath + ".mp3"), 'rb') as f:
-                    st.success('Successful! Download Now ⬇')
-                    st.download_button('Download Mp3', f, file_name=yt.title + '.mp3')
+                    with open(os.path.join(basePath + ".mp3"), 'rb') as f:
+                        st.success('Successful! Download Now ⬇')
+                        st.download_button('Download Mp3', f, file_name=yt.title + '.mp3')
 
-    except RegexMatchError:
-        st.warning('Please input a valid URL!')
+        except RegexMatchError:
+            st.warning('Please input a valid URL!')
+

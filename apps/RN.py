6f37c20df030191507b0a47c377c875cee23a2d0
Eric Germino <65765604+rikku-02@users.noreply.github.com>
2022-05-20 13:33:21 +0000

    Convert to Mp4 Implemented

--- apps/RN.py
+++ apps/RN.py
@@ -6,8 +6,8 @@ def app():
        def Release_Notes():
            Side_Bar_Nav = 'Implemented at the Top Left ✅'
            Convert_to_MP3 = 'Implemented ✅'
+           Convert_to_MP4 = "Implemented ✅"
            YouTube_Pro_No_ADS = 'Soon 😈'
-           YouTube_to_MP4 = "We're working on that.. ☕"
            Music_Stream = 'Hmm.. 🧐'
            """
 
--- apps/ytmp4.py
+++ apps/ytmp4.py
@@ -1,5 +1,41 @@
 import streamlit as st
+from pytube import YouTube
+from pytube.exceptions import RegexMatchError
+import datetime
 
 
 def app():
     st.header('Convert to MP4 📹')
+    filePath = ''
+
+    url = st.text_input(label='URL: ')
+    convert = st.button('Convert')
+
+    if url == '':
+        pass
+
+    else:
+        try:
+            yti = YouTube(url)
+            st.subheader(yti.title)
+            st.image(yti.thumbnail_url, width=200)
+            sec = yti.length
+            res = datetime.timedelta(seconds=sec)
+            st.text(f'Duration: {res} ')
+            # convert = st.button('Convert')
+
+            if convert:
+                with st.spinner('Converting...'):
+                    yt = YouTube(url)
+
+                    yt.streams.filter(only_audio=True)
+                    stream = yt.streams.get_by_itag(22)
+
+                    stream.download(filePath)
+
+                    with open(yt.title + '.mp4', 'rb') as f:
+                        st.success('Successful! Download Now ⬇')
+                        st.download_button('Download Mp4', f, file_name=yt.title + '.mp4')
+
+        except RegexMatchError:
+            st.warning('Please input a valid URL!')

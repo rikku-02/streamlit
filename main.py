3a2aa4b73a11f44d02c8fc0b4151666b1ed106c7
Eric Germino <65765604+rikku-02@users.noreply.github.com>
2022-05-20 14:43:48 +0000

    test

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import streamlit as st
+from pytube import YouTube
+url = st.text_input(label='URL: ')
+convert = st.button('Convert')
 
+
+if convert:
+  yt = YouTube(url)
+  stream = yt.streams.get_by_itag(22)
+  streams.download()

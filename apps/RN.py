9fd7a20c68f074d9992242269d37711c6aca0d77
Eric Germino <65765604+rikku-02@users.noreply.github.com>
2022-05-20 15:04:39 +0000

    Latest Release

--- apps/RN.py
+++ apps/RN.py
@@ -4,7 +4,7 @@ import streamlit as st
 def app():
     code = """
        def Release_Notes():
-           Side_Bar_Nav = 'Implemented at the Top Left ✅'
+           Side_Bar_Nav = 'Implemented at the Top-Left ✅'
            Convert_to_MP3 = 'Implemented ✅'
            Convert_to_MP4 = 'Implemented ✅'
            YouTube_Pro_No_ADS = 'Soon 😈'

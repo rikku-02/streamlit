138ddb0756a816cfe6118fdf823af540c599584a
Eric Germino <65765604+rikku-02@users.noreply.github.com>
2022-05-21 08:41:03 +0000

    debug

--- app/create.py
+++ app/create.py
@@ -93,13 +93,6 @@ def app():
                     get_concat_tile_repeat(im_s, vertical_row, horizontal_row).save('concat.jpg')
                     st.image('concat.jpg')
 
-                    with open("concat.png", "rb") as file:
-                      btn = st.download_button(
-                        label="Download image",
-                        data=file,
-                        file_name="concat.jpg",
-                        mime="image/png")
-
                     with open(txt + '.pdf', "wb") as f:
                         f.write(img2pdf.convert('concat.jpg'))
 
@@ -110,6 +103,13 @@ def app():
                                            data=PDF,
                                            file_name=txt + '.pdf',
                                            mime='application/octet-stream')
+
+                    with open("concat.png", "rb") as file:
+                      btn = st.download_button(
+                        label="Download image",
+                        data=file,
+                        file_name="concat.jpg",
+                        mime="image/png")
 
             except ValueError:
                 st.warning('0 Value is not allowed to concatenate the image.')

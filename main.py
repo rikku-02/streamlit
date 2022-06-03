1c31dd69afcbe90dfa055193def8c9a3ee2d7977
Eric Germino <65765604+rikku-02@users.noreply.github.com>
2022-06-03 09:13:24 +0000

    test

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import streamlit as st
-from pytube import YouTube
-url = st.text_input(label='URL: ')
-convert = st.button('Convert')
+from getmac import get_mac_address
 
+mac = get_mac_address()
 
-if convert:
-  yt = YouTube(url)
-  stream = yt.streams.get_by_itag(22)
-  streams.download()
+st.write(mac)

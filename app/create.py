8ad2924fb302d5c120ffb32990d5d5046775fc33
Eric Germino <65765604+rikku-02@users.noreply.github.com>
2022-06-25 11:37:19 +0000

    Add Font: Sakurata

--- app/create.py
+++ app/create.py
@@ -9,9 +9,9 @@ def app():
 
         with col1:
             st.header('Text Style')
-            font_style = st.selectbox('Font Style:', ('Sakurata', 'Dream_Catcher', 'Nyctographic', 'Thunderblack', 'Perpetrator_Italic', 'Perpetrator_Regular',
+            font_style = st.selectbox('Font Style:', ('Dream_Catcher', 'Nyctographic', 'Thunderblack', 'Perpetrator_Italic', 'Perpetrator_Regular',
                 'Sparkles',
-                'Translator'))
+                'Translator', 'Sakurata'))
             font_Size = st.number_input('Font Size: ', min_value=None, max_value=None, value=20)
             font_color = st.color_picker('Font Color:', '#fff')
 
